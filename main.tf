649674984053d5e80ba84380d58f530e57537465
tdexpeter <117410568+tdexpeter@users.noreply.github.com>
2023-02-08 20:55:55 -0600

    push 8

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0b5eea76982371e91"
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
